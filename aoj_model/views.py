23b782e85a0fbfe1767402c55d94ab004d34abb9
Nontachai Yoothai <nontachai.y@gmail.com>
2020-04-15 20:53:51 +0700

    edit_01

--- aoj_model/views.py
+++ aoj_model/views.py
@@ -13,45 +13,55 @@ import transformer_model
 import os
 
 @csrf_exempt
-def select_aoj_request(request):
-    aoj_code = json.loads(request.GET["code"])
+def select_aoj_request(request): # function for select by location and send data to map
     
-    tr_c2 = os.path.abspath(os.path.join(os.path.dirname(transformer_model.__file__), 'data','DS_Transformer_C2_Pro.shp'))
+    aoj_code = json.loads(request.GET["code"]) # get AOJ code from html 
+    aoj = Aoj_C2.objects.all().filter(code=aoj_code) # AOJ queryset from Models
+    aoj_poly = aoj[0].geom # aoj_poly is Geometry Dataset 
+    tr = Transformer_C2.objects.all().filter(geom__within = aoj_poly) # Transformer in AOJ Polygon
     
-    aoj = Aoj_C2.objects.all().filter(code=aoj_code)
-    aoj_poly = aoj[0].geom
-    print(type(aoj_poly))
-    
-    tr = Transformer_C2.objects.all().filter(geom__within = aoj_poly)
-    print(len(tr))
-    
+    # built AOJ GeoJSON object and add centroid in fields cent.
     qs = Aoj_C2.objects.all().filter(code=aoj_code).annotate(cent=AsGeoJSON(Centroid('geom')))
     for i in qs.values():
         print(json.loads(i.get("cent")).get("coordinates"))
         centroid_aoj = json.loads(i.get("cent")).get("coordinates")
     
-    res = serialize(
+    res = serialize( # pack GeoJSON in res variable.
         'geojson',
         Aoj_C2.objects.all().filter(code=aoj_code),
         fields=('geom', ),
     )
     
+    tr_geojson = serialize(
+        'geojson',
+        Transformer_C2.objects.all().filter(geom__within = aoj_poly),
+        fields=('geom', ),
+    )
+  
     result = []
     result_dict = {}
     for i in tr :
-        # point_tr.append(i.geom.geojson)
-        # pea_no.append(i.facilityid)
-        # print(type(i.geom.json))
+       
         json_obj = json.loads(i.geom.json)
+        # Edit JSON Schema if you want addition more fields.
         result_dict.update({"peano": i.facilityid, "ratekva": i.ratekva, 
                             "gistag" : i.tag, "feederID": i.feederid, "phase" : i.phasedesig,
                             "name" : i.name, "flag" : i.flag,
                             "impact" : i.impact, "baseload" : i.loadProfile_base,
                             "evload" : i.loadProfile_ev, "coordinate": json_obj["coordinates"]})
         
+     
         result.append(result_dict)
         result_dict = {}
+        
+    # data = json.loads(tr_geojson)
+    # with open("./transformer_geojson.json", 'w') as f:
+    #     json.dump(data, f)
     
-    print(result)   
-    return JsonResponse({"data": res, "coordinate_cent" : centroid_aoj, \
-                            "point": json.dumps(result)})
+    return JsonResponse({
+                         "data": res, \
+                         "coordinate_cent" : centroid_aoj, \
+                         "point": json.dumps(result)
+                         
+                         })
+    
